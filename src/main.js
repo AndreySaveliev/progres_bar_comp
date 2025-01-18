b7cf08365a1d16fa289c46d866af1e3f994ec782
Andrey <viiixx@yandex.ru>
2025-01-18 13:46:01 +0300

    build

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-import ProgressBar from "./Progress";
-const temp = document.querySelector("#progress_template");
-
-const progressClone = temp.content.cloneNode(true);
-const progress = new ProgressBar(progressClone);
-const barEl = progress.generateBar();
-document.querySelector("#app").appendChild(barEl);
--- src/utils/debounce.js
+++
@@ -1,8 +0,0 @@
-export function debounce(fn, delay) {
-  let timeoutId;
-  return (...args) => {
-    clearTimeout(timeoutId);
-    // timeoutId = setTimeout(() => fn.apply(this, args), delay);
-    timeoutId = setTimeout(() => fn(...args), delay);
-  };
-}
